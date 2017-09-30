091915fd44eac4f56b135801ce216af5679d7f3f
Masaya Uno <uno@nlp.ist.i.kyoto-u.ac.jp>
2017-09-30 20:00:55 +0900

    templates/template.py: modified

--- templates/template.py
+++ templates/template.py
@@ -1,12 +1,6 @@
 # coding: utf-8
 
-from __future__ import print_function
-from __future__ import unicode_literals
-from __future__ import division
-from __future__ import absolute_import
 import math
-import string
-import itertools
 import fractions
 import heapq
 import collections
@@ -14,7 +8,17 @@ import re
 import array
 import bisect
 
-def array2d(d1, d2, init = None):
-    return [[init for _ in range(d2)] for _ in range(d1)]
+from collections import Counter, defaultdict
 
-a, b = map(int, input().split(" "))
+
+def array2d(dim1, dim2, init=None):
+    return [[init for _ in range(dim2)] for _ in range(dim1)]
+
+
+def main():
+    N = int(input())
+    W, H = map(int, input().split(" "))
+
+
+if __name__ == "__main__":
+    print(main())
